20b39eb3d5aeeebbc43ef43487d48b5fbaa4a7ad
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-12-04 09:45:18 -0300

    feat: creates tema controller with its operations

--- src/tema/controller/tema.controller.ts
+++ src/tema/controller/tema.controller.ts
@@ -1,6 +1,55 @@
-import { Controller } from "@nestjs/common";
+import { 
+    Body, 
+    Controller, 
+    Delete, 
+    Get, 
+    HttpCode, 
+    HttpStatus, 
+    Param, 
+    ParseIntPipe, 
+    Post, 
+    Put 
+} from "@nestjs/common";
+import { TemaService } from "../service/tema.service";
+import { Tema } from "../entities/tema.entity";
 
 @Controller('/temas')
 export class TemaController {
-    // Implementação do controlador de Tema
+    constructor(private readonly temaService: TemaService) {}
+
+    @Post()
+    @HttpCode(HttpStatus.OK)
+    async create(@Body() tema: Tema): Promise<Tema> {
+        return await this.temaService.create(tema);
+    }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    async findAll(): Promise<Tema[]> {
+        return await this.temaService.findAll();
+    }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    async findOne(@Param('id', ParseIntPipe) id: number): Promise<Tema> {
+        return await this.temaService.findById(id);
+    }
+
+    @Get('/descricao/:descricao')
+    @HttpCode(HttpStatus.OK)
+    async findByDescricao(@Param('descricao') descricao: string): Promise<Tema[]> {
+        return await this.temaService.findByDescricao(descricao);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    async update(@Body() tema: Tema): Promise<Tema> {
+        return await this.temaService.update(tema);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        await this.temaService.delete(id);
+    }
 }

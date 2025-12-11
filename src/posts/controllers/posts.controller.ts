ac69cc6cbd6629ec324ac24dc077a41f5d7cd492
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-12-11 08:55:14 -0300

    feat: adds jwt guard on controllers

--- src/posts/controllers/posts.controller.ts
+++ src/posts/controllers/posts.controller.ts
@@ -8,48 +8,56 @@ import {
     Param, 
     ParseIntPipe, 
     Post, 
-    Put 
+    Put, 
+    UseGuards
 } from "@nestjs/common";
 import { PostsService } from "../services/posts.service";
 import { CrudRepositoryContract } from "../../common/interfaces/crudRepositoryContract";
 import { DeleteResult } from "typeorm";
 import { Posts } from "../entities/posts.entity";
+import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 
 @Controller('/post')
 export class PostsController implements CrudRepositoryContract<Posts> {
 
     constructor(private readonly postsService: PostsService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     @HttpCode(HttpStatus.OK)
     async create(@Body() newPost: Posts): Promise<Posts> {
         return await this.postsService.create(newPost);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get()
     @HttpCode(HttpStatus.OK)
     async findAll(): Promise<Posts[]> {
         return await this.postsService.findAll();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/:id')
     @HttpCode(HttpStatus.OK)
     async findById(@Param('id', ParseIntPipe) id: number): Promise<Posts | null> {
         return await this.postsService.findById(id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/title/:title')
     @HttpCode(HttpStatus.OK)
     async findAllByTitle(@Param('title') title: string): Promise<Posts[]> {
         return await this.postsService.fetchPostsByTitle(title);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Put()
     @HttpCode(HttpStatus.OK)
     async update(@Body() updatedPost: Posts): Promise<Posts> {
         return await this.postsService.update(updatedPost);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete('/:id')
     @HttpCode(HttpStatus.NO_CONTENT)
     async delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
--- src/theme/controller/theme.controller.ts
+++ src/theme/controller/theme.controller.ts
@@ -8,53 +8,62 @@ import {
     Param, 
     ParseIntPipe, 
     Post, 
-    Put 
+    Put, 
+    UseGuards
 } from "@nestjs/common";
 import { ThemeService } from "../service/theme.service";
 import { Theme } from "../entities/theme.entity";
 import { CrudRepositoryContract } from "../../common/interfaces/crudRepositoryContract";
 import { DeleteResult } from "typeorm";
+import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 
 @Controller('/theme')
 export class ThemeController implements CrudRepositoryContract<Theme> {
     constructor(private readonly themeService: ThemeService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     @HttpCode(HttpStatus.OK)
     async create(@Body() theme: Theme): Promise<Theme> {
         return await this.themeService.create(theme);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get()
     @HttpCode(HttpStatus.OK)
     async findAll(): Promise<Theme[]> {
         return await this.themeService.findAll();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/:id')
     @HttpCode(HttpStatus.OK)
     async findById(@Param('id', ParseIntPipe) id: number): Promise<Theme> {
         return await this.themeService.findById(id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/description/:description')
     @HttpCode(HttpStatus.OK)
     async findByDescription(@Param('description') description: string): Promise<Theme[]> {
         return await this.themeService.findByDescription(description);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/name/:name')
     @HttpCode(HttpStatus.OK)
     async findByName(@Param('name') name: string): Promise<Theme[]> {
         return await this.themeService.findByName(name);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Put()
     @HttpCode(HttpStatus.OK)
     async update(@Body() theme: Theme): Promise<Theme> {
         return await this.themeService.update(theme);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete('/:id')
     @HttpCode(HttpStatus.NO_CONTENT)
     async delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {

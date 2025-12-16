956ceed26ce7acb5f2201d42376b53ee4f1f4501
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2025-12-16 18:30:20 -0300

    feat: creates deploy configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
-import { Posts } from './posts/entities/posts.entity';
-import { Theme } from './theme/entities/theme.entity';
 import { ThemeModule } from './theme/theme.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { User } from './user/entities/user.entity';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
+import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 @Module({
   imports: [
@@ -18,18 +17,10 @@ import { AppController } from './app.controller';
     }),
 
     TypeOrmModule.forRootAsync({
+      useClass: process.env.NODE_ENV === 'production'
+        ? ProdService
+        : DevService,
       imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: configService.get<string>('DB_TYPE') as 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        synchronize: configService.get<string>('NODE_ENV') === 'development',
-        entities: [Posts, Theme, User],
-      }),
     }),
     PostsModule,
     ThemeModule,
